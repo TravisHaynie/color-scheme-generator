fdebeb96e91fdc38d6ae3e774329c78e9e1b8593
unknown <travihay@gmail.com>
2025-06-18 16:37:16 -0500

    added more html css and js

--- index.js
+++ index.js
@@ -1,40 +1,39 @@
+
+
 const mainEL = document.getElementById("main-content");
 const colorSchemeButton =document.getElementById("button");
-colorSchemeButton.addEventListener('click', function(e) {
-    e.preventDefault();
-        fetchColorScheme()
+const hexValuesEl = document.getElementById('hex-value');
 
-   
+colorSchemeButton.addEventListener('click', function(e) {
+    e.preventDefault()
+    fetchColorScheme()
 })
 
 function fetchColorScheme() {
-
     const inputColor = document.getElementById("input");
     const oneHex = inputColor.value.slice(1);
+    const schemeMode = document.getElementById('mode-select');
+    const mode = schemeMode.value;
   
-    
-    fetch(`https://www.thecolorapi.com/scheme?hex=${oneHex}`)
+    fetch(`https://www.thecolorapi.com/scheme?hex=${oneHex}&mode=${mode}`)
     .then(response => response.json())
     .then(data => {
         mainEL.innerHTML = '';
+        hexValuesEl.innerHTML = '';
 
         for(let color of data.colors) {
-            console.log(color)
-            const hexValue = color.hex.value
+            const hexValue = color.hex.value;
             const nameValue = color.name.value;
-            const div = document.createElement('div');
-            div.classList.add('color-list')
-            div.style.backgroundColor = hexValue
-            div.style.height ='500px'
-            div.innerText = `${nameValue}`
+            const div1 = document.createElement('div');
+            div1.classList.add('color-list');
+            div1.style.backgroundColor = hexValue;
+            div1.style.height ='500px';
+            div1.innerText = `${nameValue}`;
+            mainEL.appendChild(div1);
 
-           
-            
-
-            mainEL.appendChild(div)
-       
-
-   
+            const div2 = document.createElement('div');
+            div2.innerText = `${hexValue}`;
+            hexValuesEl.appendChild(div2);
         }
     })
 }
