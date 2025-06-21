66d7436bac888dc323c3f6d89893420c134caa3d
unknown <travihay@gmail.com>
2025-06-21 10:27:22 -0500

    added final js with copty to clipboard functionality

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-
-
 const mainEL = document.getElementById("main-content");
 const colorSchemeButton =document.getElementById("button");
 const hexValuesEl = document.getElementById('hex-value');
@@ -12,10 +10,9 @@ colorSchemeButton.addEventListener('click', function(e) {
 function fetchColorScheme() {
     const inputColor = document.getElementById("input");
     const oneHex = inputColor.value.slice(1);
-    const schemeMode = document.getElementById('mode-select');
-    const mode = schemeMode.value;
+    const schemeMode = document.getElementById('mode-select').value;
   
-    fetch(`https://www.thecolorapi.com/scheme?hex=${oneHex}&mode=${mode}`)
+    fetch(`https://www.thecolorapi.com/scheme?hex=${oneHex}&mode=${schemeMode}`)
     .then(response => response.json())
     .then(data => {
         mainEL.innerHTML = '';
@@ -23,17 +20,31 @@ function fetchColorScheme() {
 
         for(let color of data.colors) {
             const hexValue = color.hex.value;
-            const nameValue = color.name.value;
             const div1 = document.createElement('div');
+
             div1.classList.add('color-list');
             div1.style.backgroundColor = hexValue;
             div1.style.height ='500px';
-            div1.innerText = `${nameValue}`;
             mainEL.appendChild(div1);
+            div1.onclick = function() {
+                navigator.clipboard.writeText(`${hexValue}`)
+                alert('copied hexValue to clipboard')
+            }
 
             const div2 = document.createElement('div');
+
+            
             div2.innerText = `${hexValue}`;
             hexValuesEl.appendChild(div2);
+            div2.id = "hexvalues"
+            div2.onclick = function() {
+                navigator.clipboard.writeText(`${hexValue}`)
+                alert('copied hexValue to clipboard')
+            }
         }
     })
 }
+
+
+
+
